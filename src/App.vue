925c80a42fda0edc9724ce202f5b79b697dfc136
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-10-26 22:33:38 +0300

    App creation first steps

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <HeaderSection />
+    <HomeSection />
+    <LinksSection />
+    <StatisticsSection />
+    <BoostSection />
+    <FooterSection />
+  </main>
+</template>
+
+<script setup>
+import HeaderSection from './components/HeaderSection.vue'
+import HomeSection from './components/HomeSection.vue'
+import LinksSection from './components/LinksSection.vue'
+import StatisticsSection from './components/StatisticsSection.vue'
+import BoostSection from './components/BoostSection.vue'
+import FooterSection from './components/FooterSection.vue'
+</script>
+
+<style scoped></style>
