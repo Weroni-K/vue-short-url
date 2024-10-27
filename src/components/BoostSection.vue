efbf90d3aa39c4403719580e736e8a4a3865c94e
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-10-27 12:57:16 +0200

    added styles, assets, first component

---
+++ src/components/BoostSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="boost-section">
+    <h2>Boost your links today</h2>
+    <button>Get Started</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+#boost-section {
+  height: 200px;
+  background-color: var(--color-primary-violet);
+  background-image: url(/src/assets/icons/bg-boost-desktop.svg);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+</style>
