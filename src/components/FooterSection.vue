6d9c080ff75c1b059fb2df7eb4e60e06e17d3d36
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-10-28 21:38:43 +0200

    added new components

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,150 @@
+<template>
+  <div id="footer-section">
+    <div class="logo">
+      <img
+        src="/src/assets/icons/logo.svg"
+        alt="Shortly logo"
+        class="shortly-logo-footer"
+      />
+    </div>
+
+    <div class="info-links-grid">
+      <div
+        v-for="(column, titleIndex) in footerColumns"
+        :key="titleIndex"
+        class="info-links-column"
+      >
+        <h3>{{ column.title }}</h3>
+        <ul>
+          <li v-for="(link, linkIndex) in column.links" :key="linkIndex">
+            <a :href="link.url">{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="social-links">
+      <a
+        v-for="(social, socialIndex) in socialLinks"
+        :key="socialIndex"
+        :href="social.url"
+        class="social-link"
+        target="_blank"
+      >
+        <img :src="social.logo" alt="Social media logo" class="social-icon" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import FacebookIcon from './../assets/icons/icon-facebook.svg'
+import TwitterIcon from './../assets/icons/icon-twitter.svg'
+import PinterestIcon from './../assets/icons/icon-pinterest.svg'
+import InstagramIcon from './../assets/icons/icon-instagram.svg'
+
+const footerColumns = ref([
+  {
+    title: 'Features',
+    links: [
+      { text: 'Link Shortening', url: '/link-shortening' },
+      { text: 'Branded Links', url: '/branded-links' },
+      { text: 'Analytics', url: '/analytics' },
+    ],
+  },
+  {
+    title: 'Resources',
+    links: [
+      { text: 'Blog', url: '/blog' },
+      { text: 'Developers', url: 'https://github.com/Weroni-K' },
+      { text: 'Support', url: '/support' },
+    ],
+  },
+  {
+    title: 'Company',
+    links: [
+      { text: 'About', url: '/about' },
+      { text: 'Our Team', url: '/our-team' },
+      { text: 'Careers', url: '/careers' },
+      { text: 'Contact', url: '/contact' },
+    ],
+  },
+])
+
+const socialLinks = ref([
+  { logo: FacebookIcon, url: 'https://facebook.com' },
+  { logo: TwitterIcon, url: 'https://twitter.com' },
+  { logo: PinterestIcon, url: 'https://pinterest.com' },
+  { logo: InstagramIcon, url: 'https://instagram.com' },
+])
+</script>
+
+<style scoped>
+#footer-section {
+  background-color: var(--color-neutral-dark-violet);
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 60px 120px;
+  gap: 100px;
+}
+.shortly-logo-footer {
+  filter: invert(80%) sepia(100%) saturate(350%) hue-rotate(160deg)
+    brightness(280%);
+}
+
+.info-links-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 128px);
+  gap: 60px;
+}
+
+.info-links-column h3 {
+  margin-bottom: 24px;
+  font-size: 18px;
+}
+
+.info-links-column ul {
+  list-style: none;
+  padding: 0;
+}
+
+.info-links-column li {
+  margin-bottom: 5px;
+}
+
+.info-links-column a {
+  text-decoration: none;
+  color: var(--color-neutral-grey);
+}
+
+.info-links-column a:hover {
+  color: var(--color-primary-cyan);
+}
+
+.social-links {
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+}
+
+.social-icon {
+  width: 24px;
+  height: 24px;
+}
+
+.social-link:hover .social-icon {
+  filter: invert(80%) sepia(100%) saturate(350%) hue-rotate(160deg)
+    brightness(280%);
+}
+
+/* @media (max-width: 720px) {
+  .info-links-grid {
+    grid-template-columns: 1fr;
+  }
+  .social-links {
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 20px;
+  }
+} */
+</style>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="header-section">
+    <div class="navbar">
+      <div class="navbar-left">
+        <div class="logo">
+          <img
+            src="/src/assets/icons/logo.svg"
+            alt="Shortly logo"
+            class="shortly-logo-footer"
+          />
+        </div>
+        <div class="menu-links">
+          <ul>
+            <li v-for="(link, index) in menuLinks" :key="index">
+              <a :href="link.url">{{ link.text }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="menu-buttons">
+        <a>Login</a>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const menuLinks = ref([
+  { text: 'Features', url: '/features' },
+  { text: 'Pricing', url: '/pricing' },
+  { text: 'Resources', url: '/resources' },
+])
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  flex-direction: row;
+  max-width: 1080px;
+  margin: auto;
+  padding: 48px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.navbar-left {
+  display: flex;
+  gap: 60px;
+  align-items: center;
+}
+
+.logo img {
+  display: block;
+}
+
+.menu-links ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.menu-links li {
+  margin-right: 36px;
+}
+
+.menu-links a,
+.menu-buttons a {
+  text-decoration: none;
+  color: var(--color-neutral-grey-violet);
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.menu-links a:hover,
+.menu-buttons a:hover {
+  color: var(--color-neutral-dark-violet);
+}
+
+.menu-buttons button {
+  margin-left: 36px;
+  padding: 10px 20px;
+  color: var(--color-neutral-white);
+}
+</style>
